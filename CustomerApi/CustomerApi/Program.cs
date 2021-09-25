86f349be16dfe1fbf214877191cbac793f54da80
Maciej Zelwak <maciej.zelwak@gmail.com>
2021-09-25 16:48:39 +0200

    test

--- CustomerApi/CustomerApi/Program.cs
+++ CustomerApi/CustomerApi/Program.cs
@@ -16,6 +16,7 @@ namespace CustomerApi
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                
                 });
         }
     }
