c0da1a880e3f89332049e6b479adebcf31de5bb5
wolfgangofner@aon.at <WolfgangOfner@aon.at>
2022-01-29 15:26:11 +0100

    updated swagger config

--- CustomerApi/CustomerApi/Startup.cs
+++ CustomerApi/CustomerApi/Startup.cs
@@ -155,7 +155,11 @@ namespace CustomerApi
 
             app.UseHttpsRedirection();
             app.UseSwagger();
-            app.UseSwaggerUI();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.RoutePrefix = string.Empty;
+            });
             app.UseRouting();
 
             app.UseMetricServer();
--- OrderApi/OrderApi/Startup.cs
+++ OrderApi/OrderApi/Startup.cs
@@ -152,7 +152,11 @@ namespace OrderApi
 
             app.UseHttpsRedirection();
             app.UseSwagger();
-            app.UseSwaggerUI();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.RoutePrefix = string.Empty;
+            });
             app.UseRouting();
 
             app.UseMetricServer();
